92cc8c8dae149a7c6ccf6f291b02b74c80e97b12
Leah Kerry <leah.kerry@tufts.edu>
2025-12-21 17:31:57 -0500

    created readme and ensure https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // NOTE: added this. 
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
