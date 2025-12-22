32b6cc05f8dfc8003d0a5d0a771c66ee095b520c
Leah Kerry <leah.kerry@tufts.edu>
2025-12-22 12:21:54 -0500

    unit tests for HTTP status codes

--- tests/Feature/TodoApiTest.php
+++ tests/Feature/TodoApiTest.php
@@ -9,6 +9,8 @@ class TodoApiTest extends TestCase
 {
     use RefreshDatabase;
 
+    /******* GET tests *******/
+
     public function test_can_get_all_todos()
     {
         Todo::factory()->count(3)->create();
@@ -19,15 +21,50 @@ class TodoApiTest extends TestCase
                  ->assertJsonCount(3);
     }
 
+    public function test_can_get_one_todo()
+    {
+        $todo = Todo::factory()->create();
+
+        $response = $this->getJson("/api/todos/{$todo->id}");
+
+        $response->assertStatus(200);
+
+    }
+
+    public function test_invalid_get_one_todo()
+    {
+        $response = $this->getJson("/api/todos/-1");
+
+        $response->assertStatus(404);
+    }
+
+    /******* POST tests *******/
     public function test_can_create_todo()
     {
         $data = ['title' => 'New Todo'];
-        // $data = ['title' => 'New Todo', 'due_date' => '12/25/25'];
 
         $response = $this->postJson('/api/todos', $data);
 
         $response->assertStatus(201)
                  ->assertJsonFragment($data);
+    }
+
+    public function test_invalid_create_todo()
+    {
+        $data = ['title' => ''];
+
+        $response = $this->postJson('/api/todos', $data);
+
+        $response->assertStatus(422);
+    }
+
+    public function test_nodata_create_todo()
+    {
+        $data = [];
+
+        $response = $this->postJson('/api/todos', $data);
+
+        $response->assertStatus(422);
     }
 
     public function test_can_create_date_todo()
@@ -39,6 +76,8 @@ class TodoApiTest extends TestCase
         $response->assertStatus(201)
                  ->assertJsonFragment($data);
     }
+    
+    /******* PUT tests *******/
 
     public function test_can_update_todo()
     {
@@ -51,6 +90,7 @@ class TodoApiTest extends TestCase
         $response->assertStatus(200)
                  ->assertJsonFragment($data);
     }
+
     public function test_can_update_date_todo()
     {
         $todo = Todo::factory()->create();
@@ -63,6 +103,47 @@ class TodoApiTest extends TestCase
                  ->assertJsonFragment($data);
     }
 
+    public function test_invalid_update_todo()
+    {
+        $todo = Todo::factory()->create();
+        
+        $data = ['title' => '', 'completed' => true];
+
+        $response = $this->putJson("/api/todos/{$todo->id}", $data);
+
+        $response->assertStatus(422);
+    }
+
+    public function test_remove_date_update_todo()
+    {
+        $todo = Todo::factory()->create();
+
+        $data = ['title' => 'Original Title', 'due_date' => '1222-12-12', 'completed' => true];
+        $response = $this->putJson("/api/todos/{$todo->id}", $data);
+        $response->assertStatus(200)
+                 ->assertJsonFragment($data);
+
+        $newdata = ['title' => 'New Title2', 'due_date' => null, 'completed' => true];
+
+        $newresponse = $this->putJson("/api/todos/{$todo->id}", $newdata);
+
+        $newresponse->assertStatus(200)
+                 ->assertJsonFragment($newdata);
+    }
+
+    public function test_invalid_date_update_todo()
+    {
+        $todo = Todo::factory()->create();
+
+        $data = ['title' => 'New Title', 'due_date' => 'a98sd', 'completed' => true];
+
+        $response = $this->putJson("/api/todos/{$todo->id}", $data);
+        
+        $response->assertStatus(422); 
+    }
+
+    /******* DELETE tests *******/
+
     public function test_can_delete_todo()
     {
         $todo = Todo::factory()->create();
@@ -70,5 +151,12 @@ class TodoApiTest extends TestCase
         $response = $this->deleteJson("/api/todos/{$todo->id}");
 
         $response->assertStatus(204);
+    }
+
+    public function test_delete_not_found_todo()
+    {
+        $response = $this->deleteJson("/api/todos/-1");
+
+        $response->assertStatus(404);
     }
 }
