e4a5e49ea2bfc8c6c6d046ffb831f0ee2f1b1b18
Leah Kerry <leah.kerry@tufts.edu>
2025-12-27 11:41:54 -0500

    force https in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,4 +14,11 @@ class AppServiceProvider extends ServiceProvider
         //
     }
 
+    public function boot(): void
+    {
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+    }
+
 }
